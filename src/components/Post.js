81c79cb0012f7c303961e2f166f3974595d799e6
Mateus Bezerra <mateusbzerra@gmail.com>
2019-06-19 15:58:55 -0300

    Changes on Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class Post extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.content}>
-            <View style={styles.boxHeader}>
-                <Text style={styles.boxTitle}>Aprendendo React Native</Text>
-                <Text style={styles.boxName}>Mateus Bezerra da Silva</Text>
-            </View>
-            <Text style={styles.contentText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Curabitur accumsan, mauris at aliquam imperdiet, augue ipsum convallis diam, sed tincidunt lectus metus in diam.</Text>
-        </View>
-    </View>
-    )
-  }
+	render() {
+		const { data } = this.props;
+		return (
+			<View style={styles.container}>
+				<View style={styles.content}>
+					<View style={styles.boxHeader}>
+						<Text style={styles.boxTitle}>{data.title}</Text>
+						<Text style={styles.boxName}>{data.author} </Text>
+					</View>
+					<Text style={styles.contentText}> {data.content} </Text>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#fff',
-        marginVertical:10,
-        marginHorizontal:20,
-        borderRadius:5,
-    },
-    content:{
-        padding:20,
-    },
-    boxHeader:{
-        borderBottomWidth:1,
-        borderBottomColor:'#eee',
-    },
-    boxTitle:{
-        fontSize:16,
-        fontWeight:'bold',
-        marginBottom:2,
-    },
-    boxName:{
-        fontSize:13,
-        color:'#bababa',
-        marginBottom:10,
-    },
-    contentText:{
-        marginTop:15,
-        color:'#6b6b6b'
-    }
-
-})
+	container: {
+		backgroundColor: '#fff',
+		marginVertical: 10,
+		marginHorizontal: 20,
+		borderRadius: 5
+	},
+	content: {
+		padding: 20
+	},
+	boxHeader: {
+		borderBottomWidth: 1,
+		borderBottomColor: '#eee'
+	},
+	boxTitle: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginBottom: 2
+	},
+	boxName: {
+		fontSize: 13,
+		color: '#bababa',
+		marginBottom: 10
+	},
+	contentText: {
+		marginTop: 15,
+		color: '#6b6b6b'
+	}
+});
--- src/index.js
+++ src/index.js
@@ -1,41 +1,66 @@
-import React, {Component} from 'react';
-import {ScrollView, StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Post from './components/Post';
 
-
 export default class App extends Component {
-  render() {
-    return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>GoNative App</Text>
-        </View>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-      </ScrollView>
-    );
-  }
+	state = {
+		posts: [
+			{
+				id: 1,
+				title: 'Aprendendo React Native',
+				author: 'Mateus Bezerra',
+				content:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Curabitur accumsan, mauris at aliquam imperdiet, augue ipsum convallis diam, sed tincidunt lectus metus in diam.'
+			},
+			{
+				id: 2,
+				title: 'Aprendendo React Native',
+				author: 'Mateus Bezerra',
+				content:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Curabitur accumsan, mauris at aliquam imperdiet, augue ipsum convallis diam, sed tincidunt lectus metus in diam.'
+			},
+			{
+				id: 3,
+				title: 'Aprendendo React Native',
+				author: 'Mateus Bezerra',
+				content:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Curabitur accumsan, mauris at aliquam imperdiet, augue ipsum convallis diam, sed tincidunt lectus metus in diam.'
+			},
+			{
+				id: 4,
+				title: 'Aprendendo React Native',
+				author: 'Mateus Bezerra',
+				content:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut commodo purus. Curabitur accumsan, mauris at aliquam imperdiet, augue ipsum convallis diam, sed tincidunt lectus metus in diam.'
+			}
+		]
+	};
+	render() {
+		const { posts } = this.state;
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.headerText}>GoNative App</Text>
+				</View>
+				<ScrollView>{posts.map((post) => <Post key={post.id} data={post} />)}</ScrollView>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ef7777',
-  },
-  content:{
-    backgroundColor:'#ef7777',
-    flex:1,
-  },
-  header:{
-    backgroundColor:'#fff',
-    alignItems:'center',
-    height:80,
-  },
-  headerText:{
-    fontSize:16,
-    fontWeight:'bold',
-    marginTop:45,
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#ef7777'
+	},
+	header: {
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		height: 80
+	},
+	headerText: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginTop: 45
+	}
 });
